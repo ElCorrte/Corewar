85cd41e6a6b9463ea18c1b98a4cfc0e282a88611
kzakharc <kzakharc@student.unit.ua>
2017-08-15 15:53:55 +0300

    start

---
+++ includes/asm.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   asm.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzakharc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/15 13:14:08 by kzakharc          #+#    #+#             */
+/*   Updated: 2017/08/15 13:14:11 by kzakharc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ASM_H
+#define ASM_H
+
+# include "../Lib/src/libft/libft.h"
+# include "../Lib/src/ft_printf/ft_printf.h"
+# include "../Lib/src/get_next_line/get_next_line.h"
+
+void	print_usage(int i);
+void	check_and_read(char *str);
+
+#endif
---
+++ src/print_for_validation.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_for_validation.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzakharc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/15 14:04:22 by kzakharc          #+#    #+#             */
+/*   Updated: 2017/08/15 14:04:23 by kzakharc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/asm.h"
+
+void	print_usage(int i)
+{
+	(i == 0) ? (ft_printf("Usage: ./asm [-a] <sourcefile.s>\n\t-a : Instead of "
+			"creating a .cor file, outputs a stripped and annotated version "
+								  "of the code to the standard output\n")) : 0;
+}
